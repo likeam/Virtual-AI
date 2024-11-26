a1507e1a7de3262f96a99a4ab08682256d00da5e
abdul <likeam99@gmail.com>
2024-11-27 00:48:22 +0500

    Building frontend Section

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,22 +2,29 @@ const App = () => {
   return (
     <>
       <main className=" max-w-2xl mx-auto flex gap-16 px-4 ">
-        <div className=" py-8">
+        <div className=" py-4">
           <h1 className=" py-8 text-4xl font-bold uppercase">
-            Genrate a video <br /> form a URL with power of AI
+            <span className="text-5xl">URL to Video</span>
+            <br />
+            <span className="bg-gradient-to-br from-emerald-300 from-30% to-sky-300 bg-clip-text text-transparent">
+              with power of AI
+            </span>
           </h1>
-          <form className=" border-2 rounded-full flex overflow-hidden">
+          <form className="  grid gap-2 ">
             <input
               type="url"
               placeholder="https//......."
-              className=" bg-transparent text-white  px-4 py-2 grow"
+              className=" border-2 rounded-full bg-transparent text-white  px-4 py-2 grow"
             />
-            <button type="submit" className=" bg-white text-black px-4">
+            <button
+              type="submit"
+              className=" bg-emerald-500 border-2 rounded-full text-white px-4 py-2"
+            >
               Create&nbsp;Videos
             </button>
           </form>
         </div>
-        <div className=" p-8">
+        <div className=" p-4">
           <div className=" bg-gray-200 w-[240px] h-[380px]">ab</div>
         </div>
       </main>
